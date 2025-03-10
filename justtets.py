b64d60f3b3393878595e53144ed693561f427569
cherich520 <461711017@qq.com>
2025-03-10 11:27:41 +0800

    init

--- justtets.py
+++ justtets.py
@@ -1,2 +1,11 @@
 print("hello world 123")
 print("666")
+# 九九乘法表
+def multiplication_table():
+    for i in range(1, 10):
+        for j in range(1, i + 1):
+            print(f"{j}*{i}={i * j}", end="\t")
+        print()
+
+# 调用函数打印九九乘法表
+multiplication_table()
