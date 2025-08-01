ad325c695f41148f5839c3b791ae940c2815b9f9
cherich520 <461711017@qq.com>
2025-08-01 17:26:15 +0800

    Feat/record11 (#46)
    * test
    
    * test1

---
+++ conftest.py
@@ -0,0 +1,7 @@
+# 必须在 conftest.py 用 parser.addini 注册 jwt_token
+# pytest.ini 必须配置 jwt_token
+# 之后 pytestconfig.getini("jwt_token") 才能正常读取，不会报 unknown configuration value
+def pytest_addoption(parser):
+    parser.addini("jwt_token_DingTalk", "JWT token for browser localStorage", default="")
+    parser.addini("jwt_token_email", "JWT token for browser localStorage", default="")
+    # parser.addini("jwt_token_lark", "JWT token for browser localStorage", default="")
